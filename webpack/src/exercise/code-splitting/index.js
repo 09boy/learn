82f77d99ccc6508a6f24116b46dfc2688af537f4
qinmin <qinmin@wepiao.com>
2017-01-10 18:27:26 +0800

    save code

---
+++ webpack/src/exercise/code-splitting/index.js
@@ -0,0 +1,9 @@
+// var moment = require('moment');
+import moment from 'moment';
+console.log(moment().format());
+
+// var m = require('app/test.js');
+// // console.log(m)
+
+console.log('It is work');
+console.log('bundle.js');
