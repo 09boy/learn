148655b83aaab74ba1a241c4269034b4783de906
minqin <490163309@qq.com>
2017-01-17 02:39:28 +0800

    update code at home

---
+++ webpack/framework/_bin/functions/smart-path.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _path = require('path');
+
+var ROOT = (0, _path.resolve)(__dirname, '..', '..');
+
+console.log('Root Path', ROOT);
+console.log('work root path', process.cwd());
